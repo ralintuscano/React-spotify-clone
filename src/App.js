947ee7ddec4bb90f9bc514223feac5b09318f35b
ralintuscano <ralintuscano16@student.sfit.ac.in>
2020-09-08 02:04:57 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+// import Player from "./Player";
 
 import "./App.css";
 import Login from "./Login";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(0);
@@ -15,14 +19,16 @@ function App() {
 
     if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("User is : ", user);
+      });
     }
 
     console.log("I have a token ", _token);
   }, []);
 
-  return (
-    <div className="app">{token ? <h1>I am logged In!</h1> : <Login />}</div>
-  );
+  return <div className="app">{token ? <h1>Welcome</h1> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/DataLayer.js
@@ -0,0 +1,11 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+export const DataLayerContext = createContext();
+
+const DataLayer = ({ initialState, reducer, children }) => (
+  <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </DataLayerContext.Provider>
+);
+
+export default DataLayer;
