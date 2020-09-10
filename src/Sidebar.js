a11ab8def16b21747856aa656a82d6bbe638abc5
ralintuscano <ralintuscano16@student.sfit.ac.in>
2020-09-10 23:03:40 +0530

    css added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,11 @@ import "./Sidebar.css";
 function Sidebar() {
   return (
     <div className="Sidebar">
-      <h1>I am the Sidebar</h1>
+      <img
+        className="Sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
     </div>
   );
 }
