2dea7d16dc7e8ae55150c43dc48efae8182d7f1a
ralintuscano <ralintuscano16@student.sfit.ac.in>
2020-09-17 01:40:54 +0530

    completed Header

--- src/Body.js
+++ src/Body.js
@@ -7,6 +7,15 @@ function Body({ spotify }) {
   return (
     <div className="Body">
       <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img src="" alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description...</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,21 +3,23 @@ import SearchIcon from "@material-ui/icons/Search";
 import { Avatar } from "@material-ui/core";
 
 import "./Header.css";
+import { useDataLayerValue } from "./DataLayer";
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
   return (
     <div className="header">
       <div className="header__left">
         <SearchIcon />
         <input
           type="text"
-          autofocus
+          autoFocus
           placeholder="Search for Artists, Songs, or Podcasts"
         />
       </div>
       <div className="header__right">
-        <h4>Querty</h4>
-        <Avatar src="" alt="Q" />
+        <h4>{user?.display_name}</h4>
+        <Avatar src={user?.images[0]?.url} alt="Q" />
       </div>
     </div>
   );
